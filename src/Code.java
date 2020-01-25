cff8591406b9d2918397716611b47969003f56ef
sanedroid6006 <sanedroid@gmail.com>
2020-01-25 20:46:56 +0530

    initial

--- src/Code.java
+++ src/Code.java
@@ -5,7 +5,7 @@ public class Code
 	
 	public enum Language{
 		
-		q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m,_;
+		q,w,e,r,t,y,u,i,o,p,a,s,six,f,g,h,j,k,l,z,x,c,v,b,n,m,_;
 
 		private String getstr(Language l)
 		{
